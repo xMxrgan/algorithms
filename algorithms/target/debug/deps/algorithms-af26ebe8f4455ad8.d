b2f1e2b71f2f90d09ae186ad5af7c15738fa6feb
xMxrgan <morganpicariello@posteo.net>
2025-06-29 11:47:04 +0200

    Add initial Rust project for algorithms crate

---
+++ algorithms/target/debug/deps/algorithms-af26ebe8f4455ad8.d
@@ -0,0 +1,5 @@
+/Users/morgan/Desktop/Algorithms/algorithms/target/debug/deps/libalgorithms-af26ebe8f4455ad8.rmeta: src/main.rs
+
+/Users/morgan/Desktop/Algorithms/algorithms/target/debug/deps/algorithms-af26ebe8f4455ad8.d: src/main.rs
+
+src/main.rs:
---
+++ algorithms/target/debug/deps/algorithms-df14ce3d395abf72.d
@@ -0,0 +1,5 @@
+/Users/morgan/Desktop/Algorithms/algorithms/target/debug/deps/libalgorithms-df14ce3d395abf72.rmeta: src/main.rs
+
+/Users/morgan/Desktop/Algorithms/algorithms/target/debug/deps/algorithms-df14ce3d395abf72.d: src/main.rs
+
+src/main.rs:
